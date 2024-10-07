83504b5c5a108cafc055163ee394ec551f20319a
Roikhatul Janah <snmptns@gmail.com>
2024-10-07 14:24:57 +0700

    Initial commit

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; <a href="Sosmed Anda">2023 | Roikhatul Janah</a> </span>
+        </div>
+    </div>
+</footer>
